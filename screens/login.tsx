3367c4c9123f2c9c526335a5e2bec669f3ff6e6c
noumanhafeez <noumanhafeez095@gmail.com>
2025-01-03 18:18:39 +0500

    add some changes and icon

--- screens/login.tsx
+++ screens/login.tsx
@@ -1,57 +1,72 @@
-import React, { useState } from "react";
-import { View, Text, Image, TouchableOpacity,Alert, ImageBackground } from "react-native";
+import React, {useState} from 'react';
+import {
+  View,
+  Text,
+  Image,
+  TouchableOpacity,
+  Alert,
+  ImageBackground,
+} from 'react-native';
 import styles from '../screensStyles/loginStyles';
-import { TextInput } from "react-native-gesture-handler";
-import { useNavigation } from '@react-navigation/native';
-
+import {TextInput} from 'react-native-gesture-handler';
+import {useNavigation} from '@react-navigation/native';
 
 const Login = () => {
-    const navigation = useNavigation();
-    const handleButtonPress = () => {
-        Alert.alert(
-            "Forgot Password",
-            "Password recovery instructions have been sent to your email.",
-            [{ text: "OK", onPress: () => console.log("OK Pressed") }]
-        );
-    };
-    const handleButtonPress1 = () => {
-        Alert.alert(
-            "Alert!",
-            "We don't have your account in our database. Please sign up to create account.",
-            [{ text: "OK", onPress: () => console.log("OK Pressed") }]
-        );
-    };
+  const navigation = useNavigation();
+  const handleButtonPress = () => {
+    Alert.alert(
+      'Forgot Password',
+      'Password recovery instructions have been sent to your email.',
+      [{text: 'OK', onPress: () => console.log('OK Pressed')}],
+    );
+  };
+  const handleButtonPress1 = () => {
+    Alert.alert(
+      'Alert!',
+      "We don't have your account in our database. Please sign up to create account.",
+      [{text: 'OK', onPress: () => console.log('OK Pressed')}],
+    );
+  };
 
-    return (
-        <ImageBackground
-            source={require('../assets/images/wall_2.jpg')}  // Replace with your background image path
-            style={styles.container}  // Apply the existing container style
-        >
-            <Text style={styles.text}>Log In</Text>
-            <Text style={styles.sign}>Please sign in to your existing account</Text>
-        <View style={styles.box}>
-            <View style={styles.mail}>
-                <Text style={styles.email}>Email</Text>
-                <TextInput style={styles.example} placeholder="Enter Your Email" autoCapitalize="none" autoCorrect={false}></TextInput>
-                <Text style={styles.email}>Pasword</Text>
-                <TextInput style={styles.example} placeholder="Enter Your Password" keyboardType="email-address" secureTextEntry={true}></TextInput>
-                <TouchableOpacity onPress={handleButtonPress} style={styles.button}>
-                <Text style={styles.forgot} >Forgot Password?</Text>
-                </TouchableOpacity>
-                <TouchableOpacity onPress={handleButtonPress1} style={styles.button1}>
-                    <Text style={{ color: "white", fontSize: 15, fontWeight: "700" }}>
-                        Log In
-                    </Text>
-                </TouchableOpacity>
-                <Text style={styles.signup}>Don't have an account?</Text>
-                <TouchableOpacity onPress={ ()=> navigation.navigate("signup") } style={styles.button}>
-                <Text style={styles.up} >Sign Up</Text>
-                </TouchableOpacity>
-            </View>
-            
+  return (
+    <ImageBackground
+      source={require('../assets/images/wall_2.jpg')} // Replace with your background image path
+      style={styles.container} // Apply the existing container style
+    >
+      <Text style={styles.text}>Log In</Text>
+      <Text style={styles.sign}>Please sign in to your existing account</Text>
+      <View style={styles.box}>
+        <View style={styles.mail}>
+          <Text style={styles.email}>Email</Text>
+          <TextInput
+            style={styles.example}
+            placeholder="Enter Your Email"
+            autoCapitalize="none"
+            autoCorrect={false}></TextInput>
+          <Text style={styles.email}>Pasword</Text>
+          <TextInput
+            style={styles.example}
+            placeholder="Enter Your Password"
+            keyboardType="email-address"
+            secureTextEntry={true}></TextInput>
+          <TouchableOpacity onPress={handleButtonPress} style={styles.button}>
+            <Text style={styles.forgot}>Forgot Password?</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={handleButtonPress1} style={styles.button1}>
+            <Text style={{color: 'white', fontSize: 15, fontWeight: '700'}}>
+              Log In
+            </Text>
+          </TouchableOpacity>
+          <Text style={styles.signup}>Don't have an account?</Text>
+          <TouchableOpacity
+            onPress={() => navigation.navigate('signup')}
+            style={styles.button}>
+            <Text style={styles.up}>Sign Up</Text>
+          </TouchableOpacity>
         </View>
-        </ImageBackground>
-    )
+      </View>
+    </ImageBackground>
+  );
 };
 
 export default Login;
--- screensStyles/homeStyles.tsx
+++ screensStyles/homeStyles.tsx
@@ -1,78 +1,76 @@
-import { StyleSheet} from 'react-native';
+import {Platform, StyleSheet} from 'react-native';
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor :"#dfbdfc",
-      alignItems: "center",
-    },
-    imgStyle: {
-      marginTop:150,
-      height:330,
-      width:330,
-      left:17
-      
-    },
-    head: { 
-      top:20,
-      fontSize: 27,
-      fontWeight: "700",
-      color: "#a949fc"
-    },
-    backgroundImage: {
-      flex: 1,
-      resizeMode: 'cover', // Ensures image covers entire screen
-      justifyContent: 'center',
+  container: {
+    flex: 1,
+    backgroundColor: '#dfbdfc',
+    alignItems: 'center',
+  },
+  imgStyle: {
+    marginTop: 110,
+    height: 330,
+    width: 330,
+    left: 17,
+  },
+  head: {
+    top: 20,
+    fontSize: 27,
+    fontWeight: '700',
+    color: '#a949fc',
+  },
+  backgroundImage: {
+    flex: 1,
+    resizeMode: 'cover', // Ensures image covers entire screen
+    justifyContent: 'center',
   },
   overlay: {
     flex: 1,
     backgroundColor: 'rgba(0,0,0,0.4)', // Optional dark overlay with opacity
     justifyContent: 'center',
     alignItems: 'center',
-},
-    titleBox: {
-      marginTop:30,
-      alignContent:"center",
-
-    },
-    title: {
-      textAlign:"center",
-      fontSize:17,
-      color: "#a949fc"
-    },
-    button: {
-      padding:22,
-      borderRadius:18,
-      backgroundColor:"#8a13f2",
-      top:95,
-      alignItems:"center"
-    },
-    button1: {
-      padding:22,
-      borderRadius:18,
-      top:96,
-      alignItems:"center"
-    },
-    dot: {
-      height:9,
-      width:9,
-      borderRadius:20,
-      marginHorizontal:20,
-      alignContent:"center",
-      backgroundColor:"#8a13f2",
-    },
-    slide: {
-      width: '40%', // Full width for each image
-      justifyContent: 'center',
-      left:40,
-      marginHorizontal:0,
-      //alignItems: 'center',
   },
-    imgstyle: {
-      width: '100%', // Ensure the image takes full width
-      height: 250, // Adjust height as needed
-      resizeMode: 'cover', // Keep aspect ratio
+  titleBox: {
+    marginTop: 30,
+    alignContent: 'center',
   },
-})
+  title: {
+    textAlign: 'center',
+    fontSize: 17,
+    color: '#a949fc',
+  },
+  button: {
+    padding: 22,
+    borderRadius: 18,
+    backgroundColor: '#8a13f2',
+    top: Platform.OS === 'ios' ? 95 : 60,
+    alignItems: 'center',
+  },
+  button1: {
+    padding: 22,
+    borderRadius: 18,
+    top: Platform.OS === 'ios' ? 95 : 55,
+    alignItems: 'center',
+  },
+  dot: {
+    height: 9,
+    width: 9,
+    borderRadius: 20,
+    marginHorizontal: 20,
+    alignContent: 'center',
+    backgroundColor: '#8a13f2',
+  },
+  slide: {
+    width: '40%', // Full width for each image
+    justifyContent: 'center',
+    left: 40,
+    marginHorizontal: 0,
+    //alignItems: 'center',
+  },
+  imgstyle: {
+    width: '100%', // Ensure the image takes full width
+    height: 250, // Adjust height as needed
+    resizeMode: 'cover', // Keep aspect ratio
+  },
+});
 
 export default styles;
